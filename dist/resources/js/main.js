cb904ea1117a56f1ca7e627f7066e8188b71a764
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-02-06 14:38:08 -0800

    if applied, this commit adds the function to add style on page scroll

---
+++ dist/resources/js/main.js
@@ -0,0 +1,20 @@
+//adds style to navbar after nav at a certain position on screen
+function navStyleOnScroll(){
+    //targets HTML element
+    const navbar = document.querySelector('#main-navbar');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50){
+            //adds background & shadow
+            navbar.classList.add('navbar-clear-bg');
+            //navbar.classList.add('shadow');
+        }else{
+            //removes background & shadow
+            navbar.classList.remove('navbar-clear-bg');
+            //navbar.classList.remove('shadow');
+        }
+    });
+}
+
+//runs function after the page has loaded
+document.addEventListener(DOMContentLoaded, navStyleOnScroll);
